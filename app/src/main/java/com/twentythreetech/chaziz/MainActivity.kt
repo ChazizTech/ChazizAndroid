c760070b6a03efbc15cfd1f687dea220610afe37
ChazizPF94 <45898787+PF94@users.noreply.github.com>
2020-03-23 23:14:08 -0400

    make "TwentyPage" Webview load 2003page, as a placeholder.
    I am not sure if i ethier go through the pain of coding or just make this a container for "chaziz.2003page.ga"

--- app/src/main/java/com/twentythreetech/chaziz/MainActivity.kt
+++ app/src/main/java/com/twentythreetech/chaziz/MainActivity.kt
@@ -3,11 +3,15 @@ package com.twentythreetech.chaziz
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.twentythreetech.chaziz.R.layout.chaziz_main
+import kotlinx.android.synthetic.main.chaziz_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(chaziz_main)
+        twentypage.loadUrl("http://2003page.ga")
         }
     }
+
+//twentypage

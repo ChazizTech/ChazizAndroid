2defa1539a2f016a03cdd2a2b755c63d4c4ff500
ChazizPF94 <45898787+PF94@users.noreply.github.com>
2020-03-23 22:41:43 -0400

    Remove code regarding tabs, thus making it launchable

--- app/src/main/java/com/twentythreetech/chaziz/MainActivity.kt
+++ app/src/main/java/com/twentythreetech/chaziz/MainActivity.kt
@@ -8,23 +8,14 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.twentythreetech.chaziz.R.layout.fragment_main
 import com.twentythreetech.chaziz.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        setContentView(fragment_main)
+        SectionsPagerAdapter(this, supportFragmentManager)
         }
     }
-}
